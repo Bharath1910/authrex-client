fdf9f1aad3a9f3fc2575ccae96b7a1fc39adca5f
roni-2004 <jaronronialbert@gmail.com>
2023-11-14 17:21:46 +0530

    Authrex Dashboard

---
+++ dashboard/app.js
@@ -0,0 +1,6 @@
+const username = document.getElementById("username");
+const userid = document.getElementById("uid");
+const api_key = document.getElementById("secret");
+const emailAuth = document.getElementById("email_auth");
+const pgpAuth = document.getElementById("pgp_auth");
+const update = document.getElementById("update");
